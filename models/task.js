03ca01c7b37383e04bc33b248a0ef9d5d4c0b383
uditsharma725 <72641703+uditsharma725@users.noreply.github.com>
2024-07-07 23:52:29 +0530

    changes for router

--- models/task.js
+++ models/task.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    dueDate: { type: String, required: true }
+    dueDate: { type: String, required: true },
+    completed: {type: Boolean, required: false}
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
--- routes/tasks.js
+++ routes/tasks.js
@@ -13,11 +13,12 @@ router.get('/', async (req, res) => {
 });
 
 // POST route to create a new task
-router.post('/', async (req, res) => {
+router.post('/tasksData', async (req, res) => {
     const newTask = new Task({
         title: req.body.title,
         description: req.body.description,
-        dueDate: req.body.dueDate
+        dueDate: req.body.dueDate,
+        completed: false
     });
     try {
         const savedTask = await newTask.save();
@@ -28,10 +29,21 @@ router.post('/', async (req, res) => {
 });
 
 // DELETE route to delete a task by id
-router.delete('/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
     try {
-        await Task.findByIdAndDelete(req.params.id);
-        res.json({ message: 'Task deleted' });
+
+        // update task as completed
+        const task = await Task.findByIdAndUpdate(
+            req.params.id,
+            { $set: { completed: true } },
+            { new: true }
+        );
+
+        if (!task) {
+            return res.status(404).send('Task not found');
+        }
+
+        res.json({ message: task });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
